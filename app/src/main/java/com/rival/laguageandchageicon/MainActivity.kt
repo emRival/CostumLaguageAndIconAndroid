529e4bee78bb128f58d923de847fcf3a08a6951c
emRival <em.rival282@gmail.com>
2022-03-21 10:43:29 +0700

    laguageandicon

---
+++ app/src/main/java/com/rival/laguageandchageicon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rival.laguageandchageicon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
